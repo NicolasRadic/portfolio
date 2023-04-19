9382abd244d26dfec305400af02b5812166bb82d
NicolasRadic <nicolas.radic05@gmail.com>
2023-04-19 14:07:12 +0200

    add some global style change and scroll to section nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,27 @@ var nav = document.getElementsByClassName("header__bar");
 let openBtn = document.getElementsByClassName("header__mobile-close");
 let closeBtn = document.getElementsByClassName("header__mobile-open");
 
-/* Set the width of the side navigation to 250px */
-
 function openNav() {
   nav[0].classList.add("active");
   openBtn[0].classList.add("open");
   closeBtn[0].classList.add("open");
 }
 
-/* Set the width of the side navigation to 0 */
-
 function closeNav() {
   nav[0].classList.remove("active");
   openBtn[0].classList.remove("open");
   closeBtn[0].classList.remove("open");
+}
+
+// ------------------------
+//  scroll function nav bar
+// ------------------------
+
+function scrollFunction(section) {
+  const element = document.getElementById(section);
+  if (window.innerWidth < 1000) {
+    element.scrollIntoView({ behavior: "smooth" });
+  } else {
+    element.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
 }
